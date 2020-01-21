a77f368af0b7a4ecbed982fd2c3221cec0e23244
Moises Goncalves <moises100hp@gmail.com>
2020-01-21 19:31:58 -0300

    atualizado

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -73,9 +73,9 @@ class _ContactPageState extends State<ContactPage> {
                     decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         image: DecorationImage(
-                            image: _editedContact.img != null ?
-                            FileImage(File(_editedContact.img)) :
-                            AssetImage("images/person.png"),
+//                            image: _editedContact.img != null ?
+//                            FileImage(File(_editedContact.img)) :
+                           image: AssetImage("images/person.png"),
                           fit: BoxFit.cover,
                         ),
                     ),
